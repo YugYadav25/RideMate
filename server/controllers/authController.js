a01c430db189bfa9ede8dba8cbcc28bb1efb08f7
Tirth Bhatt <bhatttirth18@gmail.com>
2025-11-29 01:40:09 +0530

    hotfix for critical bug

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,6 @@
-
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { validateEmail, validateRole } = require('../utils/validate');
 
 // Generate JWT Token
 const generateToken = (id) => {
